1b7c131cafdfe9c6b43696d64962fe8d3b9a5977
selfishrv <ayushicloud26@gmail.com>
2024-10-28 17:15:33 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+function loadSite(url) {
+    document.getElementById('browser').src = url;
+    addRecentlyViewed(url); // Add the loaded site to recently viewed
+}
+
+function addRecentlyViewed(url) {
+    const recentlyViewed = document.getElementById('recent-sites');
+    const listItem = document.createElement('li');
+    const link = document.createElement('a');
+    link.href = url;
+    link.target = '_blank'; // Open in a new tab
+    link.textContent = url;
+    listItem.appendChild(link);
+    recentlyViewed.appendChild(listItem);
+}
